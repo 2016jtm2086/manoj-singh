a4ea81a3316d0e5bbad26e5e8a16119ebcb5f59f
2016jtm2086 <m.s.c.thakur@gmail.com>
2016-09-10 23:48:33 +0530

    update

---
+++ manoj.py
@@ -0,0 +1,16 @@
+counter = 100
+miles = 1000
+name = "manoj"
+a = b = c = 1
+p,q = 10, "ajay"
+var1 = 5
+print a, b, name
+print var1
+str = 'Hello World!'
+print str # Prints complete string
+print str[0] # Prints first character of the string
+print str[2:5] # Prints characters starting from 3rd to 5th
+print str[2:] # Prints string starting from 3rd character
+print str * 2 # Prints string two times
+print str + "TEST"
+
